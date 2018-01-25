fc9def998c7a75932f3f2fe7b433dd4da62ef016
unknown <profile2697@gmail.com>
2018-01-25 15:09:13 +0900

    [VideoRental] move getAmount to movie
     - to increase cohesion and reduce coupling

--- VideoRental/src/main/java/step1/Movie.java
+++ VideoRental/src/main/java/step1/Movie.java
@@ -1,27 +1,52 @@
 package step1;
 
 public class Movie {
-	public static final int CHILDRENS = 2;
-	public static final int REGULAR = 0;
-	public static final int NEW_RELEASE = 1;
-	
-	private String title;
-	private int priceCode;
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
 
-	public Movie(String title, int priceCode) {
-		this.title = title;
-		this.priceCode = priceCode;
-	}
+    private String title;
+    private int priceCode;
 
-	public int getPriceCode() {
-		return priceCode;
-	}
+    public Movie(String title, int priceCode) {
+        this.title = title;
+        this.priceCode = priceCode;
+    }
 
-	public void setPriceCode(int arg) {
-		priceCode = arg;
-	}
+    public int getPriceCode() {
+        return priceCode;
+    }
 
-	public String getTitle() {
-		return title;
-	};
+    public void setPriceCode(int arg) {
+        priceCode = arg;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    ;
+
+    public double getAmount(int daysRented) {
+        double thisAmount = 0;
+
+        switch (getPriceCode()) {
+            case REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+
+            case NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+
+            case CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
 }
