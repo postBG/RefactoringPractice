1cce36d047f8435617a77ddd902b80f697cf9936
unknown <profile2697@gmail.com>
2018-01-25 15:02:52 +0900

    [VideoRental] move amountFor to Rental and rename (cohesion)

--- VideoRental/src/main/java/step1/Customer.java
+++ VideoRental/src/main/java/step1/Customer.java
@@ -28,7 +28,7 @@ class Customer {
 
 		while ( iterator.hasNext() ) {
 			Rental rental = iterator.next();
-			double thisAmount = amountFor(rental);
+			double thisAmount = rental.getAmount();
 
 			// add frequent renter points
 			frequentRenterPoints++;
@@ -47,26 +47,4 @@ class Customer {
 		return result;
 	}
 
-	private double amountFor(Rental rental) {
-		double thisAmount = 0;
-
-		switch (rental.getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-            thisAmount += 2;
-            if (rental.getDaysRented() > 2)
-                thisAmount += (rental.getDaysRented() - 2) * 1.5;
-            break;
-
-        case Movie.NEW_RELEASE:
-            thisAmount += rental.getDaysRented() * 3;
-            break;
-
-        case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (rental.getDaysRented() > 3)
-                thisAmount += (rental.getDaysRented() - 3) * 1.5;
-            break;
-        }
-		return thisAmount;
-	}
 }
